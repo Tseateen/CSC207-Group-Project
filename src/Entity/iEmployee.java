b28a544f2a38b01fe5bf2b7a40d3590eb5b076cb
ChiehAnChang <chiehan.chang@mail.utoronto.ca>
2021-10-06 16:40:50 -0400

    Add some attribute by Andy

--- src/Entity/iEmployee.java
+++ src/Entity/iEmployee.java
@@ -1,2 +1,12 @@
 public interface iEmployee {
+
+    public String getName();
+
+    public void setName(String name);
+
+    public int getId();
+
+    public int getAttendance();
+
+    public void setAttendance();
 }
