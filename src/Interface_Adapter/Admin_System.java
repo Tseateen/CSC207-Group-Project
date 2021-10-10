ff152b03aa026667b84aab9c0535391287c1550c
Cathypei <cathygetsoffer@gmail.com>
2021-10-10 18:21:36 -0400

    Change Admin_System verifyAccIsAdmin

--- src/Interface_Adapter/Admin_System.java
+++ src/Interface_Adapter/Admin_System.java
@@ -3,25 +3,25 @@ package Interface_Adapter;
 import Uses_Cases.AccountManager;
 import Uses_Cases.Verifier;
 import Uses_Cases.Pay_Manager;
-import Uses_Cases.Verifier;
 
 public class Admin_System {
     private Verifier managerEmployee;
     private AccountManager ac;
-    private Pay_Manager mangerPay;
+    private Pay_Manager managerPay;
 
     public Admin_System(){
         this.managerEmployee = new Verifier();
-        this.mangerPay = new Pay_Manager();
+        this.managerPay = new Pay_Manager();
     }
 
     public boolean verifyAccountExistence(int username, int password){
-            return managerEmployee.verifyAccountExist(username, password);
+        return managerEmployee.verifyAccountExist(username, password);
 
     }
-    public boolean verifyAuthority(int username, int password){
-        return managerEmployee.verifyAuthority(username, password);
+    public boolean verifyAccIsAdmin(int username, int password){
+        return managerEmployee.verifyisAdmin(username, password);
     }
+
 
     public void createUser(int accountNumber, int password, String name, int phone, String address, String status,
                               String department, String position, int wage, int level, int id){
