1722d148631e0c7489c8c03a1fa4afe7bef14abb
Kuan-Lin Kuo <luke900228@gmail.com>
2021-10-10 16:57:42 -0400

    Add attribute schedule in PartTimeEmployee

--- src/Entity/PartTimeEmployee.java
+++ src/Entity/PartTimeEmployee.java
@@ -1,9 +1,21 @@
 package Entity;
 
 public class PartTimeEmployee extends Employee{
-
+    private String schedule;
 
     public PartTimeEmployee(String department, String position, int wage, int level) {
         super(department, position, wage, level);
     }
+    public PartTimeEmployee(String department, String position, int wage, int level, String schedule) {
+        super(department, position, wage, level);
+        this.schedule = schedule;
+    }
+
+
+    public String getSchedule(){return this.schedule;}
+
+    public void setSchedule(String schedule){
+        this.schedule = schedule;
+    }
+
 }
