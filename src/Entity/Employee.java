4fd44caaa8436452cead524a7480fa662e4604b1
ChiehAnChang <chiehan.chang@mail.utoronto.ca>
2021-10-12 23:11:22 -0400

    Adding some docstring and stop at PartTimeEmployee

--- src/Entity/Employee.java
+++ src/Entity/Employee.java
@@ -1,21 +1,32 @@
 package Entity;
 
 public abstract class Employee{
-    /**
-     * 一个Employee的与工作相关的基本信息
-     *
-     * @param level 所持有权限的不同等级，0是所持有的最高权限
+
+    // === Instance Variables ===
+
+    // The department of this employee
+    private String department;
+    // The minimum wage of this employee
+    private int wage;
+    // The attendance of this employee
+    private int attendance;
+    // The authority level of this employee
+    private int level;
+
+    /* === Representation Invariants ===
+     * The authority level for any employee should always equal or greater than 0. i.e. level = 0,1,2,3,...
+     * The minimums wage of any employee should always equal or greater than 0.
+     * The number of attendance for any employee should always equal or greater than 0.
      */
 
-
-    private String department; //工种
-    private int wage; //工资
-    private int attendance; //
-    private int level; //
-
     /**
-    * Create an employee with their department, position, wage, and level inputed,
-    * and set attendance to 0.*/
+     * Construct a Employee, giving them the given department,
+     * wage, and level.
+     *
+     * @param department Department for this employee.
+     * @param wage Minimum wage for this employee.
+     * @param level The authority level fore this employee.
+     */
     public Employee(String department, int wage, int level){
         this.department = department;
         this.wage = wage;
@@ -23,39 +34,75 @@ public abstract class Employee{
         this.attendance = 0;
     }
 
+    // === Regular methods ===
+
+    /**
+     *
+     * @return the number of attendance for this employee.
+     */
     public int getAttendance(){
         return this.attendance;
     }
 
+    /**
+     * This method will help employee to claim attendance.
+     */
     public void setAttendance(){
         this.attendance ++;
     }
 
+    /**
+     *
+     * @return the number of wage for this employee.
+     */
     public int getWage(){
         return this.wage;
     }
 
+    /**
+     *
+     * @param wage This method can help admin setting the new minimum wage of this employee.
+     */
     public void setWage(int wage){
         this.wage = wage;
     }
 
+    /**
+     *
+     * @return This method will return the string which refers to the department of this employee.
+     */
     public String getDepartment(){
         return this.department;
     }
 
+    /**
+     *
+     * @param department This method will help admin setting the new department for this employee.
+     */
     public void setDepartment(String department){
         this.department = department;
     }
 
+    /**
+     *
+     * @return This method will return the authority level of this employee.
+     */
     public int getLevel(){
         return this.level;
     }
 
+    /**
+     *
+     * @param level This method can let admin to assign the new authority level for this employee.
+     */
     public void setLevel(int level){
         this.level = level;
     }
 
-
+    /**
+     *
+     * @return a string representation of this employee. For instance, "This is an employee in the department of IT."
+     */
     @Override
     public String toString(){
         return "This is an employee in the department of " + this.department;
