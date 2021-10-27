144dcadb22479126b44aaa538471ba36cc679d17
Xuelan Li <xuelan.li@mail.utoronto.ca>
2021-10-26 22:10:04 -0400

    Xuelan(Lily) initiated GroupManager

---
+++ src/Uses_Cases/GroupManager.java
@@ -0,0 +1,28 @@
+package Uses_Cases;
+
+import Entity.*;
+import java.util.*;
+
+public class GroupManager {
+    private LinkedList<Group> GroupMap;
+    private int totalGroup = 0;
+
+
+    public GroupManager(){
+        this.GroupMap = new LinkedList<Group>();
+    }
+
+    public int getTotalGroup() {
+        return totalGroup;
+    }
+
+    public void createGroup(Userable leader, Userable[] members){
+        totalGroup++;
+        Group newGroup = new Group(leader, members, null);
+        GroupMap.add(newGroup);
+
+    }
+
+
+
+}
