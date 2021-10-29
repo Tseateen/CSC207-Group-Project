b4cef8e95400cde9acef80f57e2c0abc6f53ee18
kyle <kylechen011223@gmail.com>
2021-10-28 23:30:34 -0400

    Kyle -> Write methods for WorkManager and Work Distributor, change WorkManager to Interface

--- src/main/UsesCases/WorkDistributor.java
+++ src/main/UsesCases/WorkDistributor.java
@@ -2,13 +2,35 @@ package main.UsesCases;
 
 import main.Entity.*;
 import java.util.*;
-public class WorkDistributor {
+public class WorkDistributor implements WorkManager{
 
-    private Map<Group, Work> workMap;
+    private HashMap<Group, Work> workMap;
 
+    public WorkDistributor(){
+        this.workMap = new HashMap<Group, Work>();
+    }
+
+    // TODO: implement this method
+    @Override
     public void createWork(String workType, String name, String id, String createTime, int level){
         WorkFactory WF = new WorkFactory();
         Work w = WF.createWork(workType, name, id, createTime, level);
+    }
+
+    // TODO: implement this method
+    @Override
+    public void deleteWork() {
+
+    }
+    // TODO: implement this method
+    @Override
+    public void extendWork(Work project) {
+
+    }
+
+    // TODO: Do we really need this?
+    public void assignWork(Work project, Group group){
+        group.setProject(project);
     }
 
 
--- src/main/UsesCases/WorkManager.java
+++ src/main/UsesCases/WorkManager.java
@@ -1,5 +1,12 @@
 package main.UsesCases;
 
-public class WorkManager {
+import main.Entity.Work;
+
+public interface WorkManager {
+    public abstract void createWork(String workType, String name, String id, String createTime, int level);
+
+    public abstract void deleteWork();
+
+    public abstract void extendWork(Work project);
 
 }
