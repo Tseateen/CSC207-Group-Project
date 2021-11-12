7ad86d9e3fba7b3cbca626fb82454c694e86ee4e
Xuelan Li <xuelan.li@mail.utoronto.ca>
2021-11-11 23:28:50 -0500

    lixuelan -> finished the docstrings in the JournalManager.java

--- src/main/UsesCases/JournalManager.java
+++ src/main/UsesCases/JournalManager.java
@@ -5,14 +5,23 @@ import main.Entity.*;
 import java.util.*;
 
 public class JournalManager {
-    // Todo: need to add docstring
+
+    // === Instance Variables ===
     private final Journal journal;
+
 
     public JournalManager (Journal journal) {
         this.journal = journal;
     }
 
 
+    /**
+     * This method will check the work in journals by the work ID.
+     *
+     * @param req the ID of the work.
+     * @return the Journal docs of the information of the project.
+     *
+     */
     private ArrayList<String> Checker(String req) {
         // req means id, if there is an id be input,means check single, designed for code smell
         ArrayList<String> JournalDocs = new ArrayList<String>();
@@ -25,17 +34,37 @@ public class JournalManager {
     }
 
 
+    /**
+     * This method will check the journal of a specific user by its id.
+     *
+     * @param id the ID of the work.
+     * @return the Journal docs of the information related to the specific user.
+     *
+     */
     public ArrayList<String> CheckOnesJournal(String id) {
         // check a specific user's journal
         return Checker(id);
     }
 
 
+    /**
+     * This method will check the journal of all the users by its id.
+     *
+     * @return the Journal docs of the information for all users.
+     *
+     */
     public ArrayList<String> CheckAll() {
         // check all info in journal
         return Checker("");
     }
 
+
+    /**
+     * This method will set the journal of for all members and check all members if they have worked on the work before.
+     *
+     * @return the ID of the journal.
+     *
+     */
     public Set<String> AllMembers() {
         // check all member who worked on this work before
         Set<String> Id_set = new HashSet<String>();
