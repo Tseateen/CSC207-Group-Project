6f388632286ecf101edf95c9dabff1438b0c2b68
Kuan-Lin Kuo <luke900228@gmail.com>
2021-11-13 18:23:06 -0500

    Debug

--- src/main/Framework/SetPersonalInfoUI.java
+++ src/main/Framework/SetPersonalInfoUI.java
@@ -31,15 +31,23 @@ public class SetPersonalInfoUI {
                             "iv) Change your address, please type 4" + "\n" +
                             "v) Change you attendance, please type 5");
             String option = keyIn.nextLine();
-            System.out.println("Please type the value you want to change:");
-            String response = keyIn.nextLine();
-            facadeSys.setPersonalInfo(option,response);
-            System.out.println("Set personal info success!");
-            System.out.println("If you want to exist setting on other information, type E. " +
-                    "Otherwise, type any other button to continuous setting.");
-            String action = keyIn.nextLine();
-            if(action.equalsIgnoreCase("E")){
-                noExit = false;
+            if (option.equals("1") || option.equals("2") || option.equals("3")
+                    || option.equals("4") || option.equals("5")) {
+                System.out.println("Please type the value you want to change:");
+                String response = keyIn.nextLine();
+
+                facadeSys.setPersonalInfo(option, response);
+                System.out.println("Set personal info success!");
+
+                System.out.println("If you want to exist setting on other information, type E. " +
+                        "Otherwise, type any other button to continuous setting.");
+                String action = keyIn.nextLine();
+                if (action.equalsIgnoreCase("E")) {
+                    noExit = false;
+                }
+            }else {
+                System.out.println("Option does not exist. Please type again!");
+                System.out.println("");
             }
         }
     }
