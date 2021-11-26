3f2c8f36ec88a05852c106d5caaa5829dfda845e
tseaten_1 <1460686504@qq.com>
2021-11-26 18:42:43 -0500

    debug
    debug

--- src/main/UsesCases/IAccountManager.java
+++ src/main/UsesCases/IAccountManager.java
@@ -1,6 +1,17 @@
 package main.UsesCases;
 
+import java.util.ArrayList;
+
 public interface IAccountManager {
     public boolean createNewAccount(String[] userinfo);
     public boolean deleteEmployee(String userID);
+    public String getUserID(String username);
+    public boolean userExists(String userID);
+    public String userLevel(String userID);
+    public ArrayList<String> infoCheck(String userID);
+    public ArrayList<String> vacationInfo(String userID);
+    public ArrayList<String> getLowerUsers(String level);
+    public boolean setUserInfo(String userID, String opt, String changeTo);
+    public boolean setEmployeeInfo(String userID, String opt, String changeTo);
+
 }
--- src/main/UsesCases/IWorkManager.java
+++ src/main/UsesCases/IWorkManager.java
@@ -3,7 +3,6 @@ package main.UsesCases;
 import main.Entity.Workable;
 
 public interface IWorkManager {
-
     public void extendWork(Workable work, String extend_date);
     public void changeState(Workable work, String new_statue);
 }
